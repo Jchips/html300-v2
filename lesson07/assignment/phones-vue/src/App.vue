838a12b2b364730ed907f51555096c81c7b94549
Jchips <jrrose@uw.edu>
2023-02-05 17:26:05 -0800

    added the accordion page

--- lesson07/assignment/phones-vue/src/App.vue
+++ lesson07/assignment/phones-vue/src/App.vue
@@ -4,9 +4,9 @@
   </header>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
     <router-link to="/companies">Companies</router-link> |
-    <router-link to="/grid">Grid</router-link>
+    <router-link to="/grid">Grid</router-link> |
+    <router-link to="/pixels">Pixels</router-link>
   </nav>
   <router-view />
 </template>
---
+++ lesson07/assignment/phones-vue/src/views/PixelsView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="pixels">
+    <!-- <header></header> -->
+  </div>
+</template>
+
+<script>
+import '/../data.js';
+document.getElementById("h1").innerHTML = "Pixels";
+export default {
+  name: "PixelsView",
+  data () {
+    return {
+      pixels: [
+        {name: "Pixel 7 Pro", id: "pixel-7-pro-features"}
+      ]
+    }
+  }
+}
+</script>
